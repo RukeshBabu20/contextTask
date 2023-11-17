cf5e35b7d5b9a7477efa63ce5e865bce8b317dbe
RukeshBabu20 <rukeshbabu20@gmail.com>
2023-11-17 23:43:50 +0530

    second commit

--- src/components/Areas.jsx
+++ src/components/Areas.jsx
@@ -4,13 +4,14 @@ import  {MyContext}  from "./Context";
 import Fare from "./FareComponent";
 import { useContext } from "react";
 import LocationSelectComponent from "./LocationSelectComponent";
+import { SelectArea } from "./UpdateList";
 
 
 const PlacesList=()=>{
 
   const myArea=useContext(MyContext);
   const areaList = SelectArea(myArea);
-  console.log(areaList);
+
 
   const [from,setFrom]=useState();
   const [to,setTo]=useState();
@@ -24,26 +25,13 @@ const PlacesList=()=>{
 
 return(
     <>
+    <h1>
     <LocationSelectComponent lists={areaList}  onSelectionChange={handleFirstInput} />  
     <LocationSelectComponent  lists={areaList} onSelectionChange={handleSecondInput} />
     <Fare from={from} to={to}/>
+    </h1>
     </>
 )
-}
-
-const SelectArea = (CityName) => {
-  if(CityName=='Hyderabad'){
-    return ['h1','h2','h3','h4','h5','h6','h7','h8'];
-  }
-  else if(CityName=='Chennai'){
-    return ['c1','c2','c3','c4','c5','c6','c7','c8'];
-  }
-  else if(CityName=='Delhi'){
-    return ['d1','d2','d3','d4','d5','d6','d7','d8'];
-  }
-  else{
-    return []
-  }
 }
 
 export  {PlacesList,SelectArea};
---
+++ src/components/UpdateList.js
@@ -0,0 +1,14 @@
+export const SelectArea = (CityName) => {
+    if(CityName=='Hyderabad'){
+      return ['h1','h2','h3','h4','h5','h6','h7','h8'];
+    }
+    else if(CityName=='Chennai'){
+      return ['c1','c2','c3','c4','c5','c6','c7','c8'];
+    }
+    else if(CityName=='Delhi'){
+      return ['d1','d2','d3','d4','d5','d6','d7','d8'];
+    }
+    else{
+      return []
+    }
+}
